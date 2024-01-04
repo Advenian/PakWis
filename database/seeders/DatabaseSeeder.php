289baf374aa7773c6b1bd325174b8a6d003bda47
pradipta <mpradipta.arya02@gmail.com>
2024-01-04 09:41:06 +0700

    implemented login and register template files to blade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@example.com',
+            'password' => bcrypt('IM A SUPERADMIN NIGGA'),
+            'role' => 'superadmin',
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Authentication\LoginController;
+use App\Http\Controllers\Authentication\LogoutController;
+use App\Http\Controllers\Authentication\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('index');
+    });
+    Route::get('/admin', function () {
+        return view('admin.index');
+    });
+
+    Route::post('/logout', [LogoutController::class, 'logout']);
 });
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register.index');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.index');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
